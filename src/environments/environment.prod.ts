f44d4eb959164568ac64cb88422260d425a5deba
Lukas <lu.heddendorp@gmail.com>
2018-10-20 11:07:38 +0200

    Move all builds to dev cloud project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBhiZBH-76bjdvUGYyXN8bfo8uxsDTEEgs',
-    authDomain: 'flatify-app.firebaseapp.com',
-    databaseURL: 'https://flatify-app.firebaseio.com',
-    projectId: 'flatify-app',
-    storageBucket: 'flatify-app.appspot.com',
-    messagingSenderId: '845342042568'
+    apiKey: 'AIzaSyBvB7zshdEIC6XBdFMro7WNjEciT3i3Wfs',
+    authDomain: 'flatify-app-dev.firebaseapp.com',
+    databaseURL: 'https://flatify-app-dev.firebaseio.com',
+    projectId: 'flatify-app-dev',
+    storageBucket: 'flatify-app-dev.appspot.com',
+    messagingSenderId: '160151363210'
   }
 };
